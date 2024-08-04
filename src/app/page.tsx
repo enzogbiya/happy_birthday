8ef51a1d5a386bb1b3f2006fc26db33eff9b1102
Enzogbiya <lipnitskiy.kirill@mail.ru>
2024-08-04 14:03:18 +0300

    add toast v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@
 import Stub from '@/components/stub'
 
 export default function Home() {
-  // @ts-ignore
   return (
-    <div>
-      <Stub date={new Date('2024-08-05T00:00:00.000Z')} />
-    </div>
+    // @ts-ignore
+    <Stub date={new Date('2024-08-05T00:00:00.000Z')} />
   )
 }
